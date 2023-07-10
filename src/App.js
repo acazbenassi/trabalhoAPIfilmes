09b97425f6ac9ab6961adaeee4f6b3ddbdb675ed
Daniel Henrique <acazbenassi@gmail.com>
2023-07-10 20:10:54 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Card from './componentes/card/card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Card></Card>
+      <p>Catalogo de Filmes</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/card/card.js
@@ -0,0 +1,12 @@
+import "./card.css";
+
+
+function Card() {
+    return (
+      <div className="Card">
+        testando
+      </div>
+    );
+  }
+  
+  export default Card;
