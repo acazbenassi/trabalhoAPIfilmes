70cd9c60fc111978f6d034bae993e171dcc000e4
Daniel Henrique <acazbenassi@gmail.com>
2023-07-15 16:30:12 -0300

    card e css

--- src/componentes/card/menu/menu.js
+++ src/componentes/card/menu/menu.js
@@ -1,12 +1,14 @@
 import "./menu.css";
 
 function Menu() {
+  
+
+
+ 
   return (
     <div className="menu">
-
-      <div className="button">
-        <button>testando</button> <button>testando</button> <button>testando</button> <button>testando</button>
-      </div>
+   
+      
 
     </div>
   );
