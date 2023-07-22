6db6fbef195774c63d09bf7a0e1810788d6b2410
Daniel Henrique <acazbenassi@gmail.com>
2023-07-22 19:00:15 -0300

    correção no export do fundo animado.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import FundoAnimado from './componentes/card/menu/FundoAnimado';
+import randomNumber from './componentes/card/menu/FundoAnimado';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
