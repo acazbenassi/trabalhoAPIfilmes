8f990e3f858e73222b914b752ea59f5f572f774e
Daniel Henrique <acazbenassi@gmail.com>
2023-07-22 18:52:26 -0300

    criação do arquivo fundoanimado.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import FundoAnimado from './componentes/card/menu/fundoanimado';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
