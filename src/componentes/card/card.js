f243bdaba2f6c28a3a9a224e02dd5d9b140da24d
Daniel Henrique <acazbenassi@gmail.com>
2023-08-04 16:09:05 -0300

    menu e card com imagens

--- src/componentes/card/card.js
+++ src/componentes/card/card.js
@@ -1,14 +1,14 @@
 import "./card.css";
-import  fundo from "../../images/fundo.png"
+import  fundo from "../../componentes/images/fundo.png"
 function Card({nome,episodes,air_date,}) {
 return (
 <div className = "card-content">
-   
+<img src ={fundo} alt="" title=""></img>  
 
 
 
  <div className ="innerCard"> <ul>
- <img src ={fundo} alt="" title=""></img>
+
    <li>Nome: {nome} </li>
    <li>Episodes: {episodes} </li>  
    <li>Air date: {air_date} </li> 
